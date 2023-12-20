559c3b22707d82247c1d9c93a225d33a1bc85867
Yuri Andriaccio <yurand2000@gmail.com>
2023-12-20 22:27:30 +0100

    Increase clarity of parse test method

--- src/parser/tokenizer/tests/mod.rs
+++ src/parser/tokenizer/tests/mod.rs
@@ -9,33 +9,45 @@ use super::Span;
 #[test]
 fn test_keyword_extraction() {
     let keywords = "if else while fn let perform with handler effect return";
-    let mut keywords = Span::from(keywords);
+    let mut keywords = StatefulParser::new(keywords);
 
-    assert_eq!(parse(&mut keywords, parse_keyword), Ok(Keyword::If));
-    assert_eq!(parse(&mut keywords, parse_keyword), Ok(Keyword::Else));
-    assert_eq!(parse(&mut keywords, parse_keyword), Ok(Keyword::While));
-    assert_eq!(parse(&mut keywords, parse_keyword), Ok(Keyword::Fn));
-    assert_eq!(parse(&mut keywords, parse_keyword), Ok(Keyword::Let));
-    assert_eq!(parse(&mut keywords, parse_keyword), Ok(Keyword::Perform));
-    assert_eq!(parse(&mut keywords, parse_keyword), Ok(Keyword::With));
-    assert_eq!(parse(&mut keywords, parse_keyword), Ok(Keyword::Handler));
-    assert_eq!(parse(&mut keywords, parse_keyword), Ok(Keyword::Effect));
-    assert_eq!(parse(&mut keywords, parse_keyword), Ok(Keyword::Return));
+    assert_eq!(keywords.parse(parse_keyword), Ok(Keyword::If));
+    assert_eq!(keywords.parse(parse_keyword), Ok(Keyword::Else));
+    assert_eq!(keywords.parse(parse_keyword), Ok(Keyword::While));
+    assert_eq!(keywords.parse(parse_keyword), Ok(Keyword::Fn));
+    assert_eq!(keywords.parse(parse_keyword), Ok(Keyword::Let));
+    assert_eq!(keywords.parse(parse_keyword), Ok(Keyword::Perform));
+    assert_eq!(keywords.parse(parse_keyword), Ok(Keyword::With));
+    assert_eq!(keywords.parse(parse_keyword), Ok(Keyword::Handler));
+    assert_eq!(keywords.parse(parse_keyword), Ok(Keyword::Effect));
+    assert_eq!(keywords.parse(parse_keyword), Ok(Keyword::Return));
 }
 
-fn parse<'a, 'b: 'a, T>(input: &'a mut Span<'b>, parser: impl FnMut(Span<'b>) -> IResult<Span<'b>, T>) -> Result<T, String> {
-    let data = std::mem::replace(input, Span::from(""));
-    let mut out_data = None;
-    let result = apply((
-        keep(&mut out_data, parser),
-        skip(space0),
-    ))(data);
+struct StatefulParser<'a> {
+    span: Span<'a>
+}
 
-    match result {
-        Ok((mut span, _)) => {
-            std::mem::swap(input, &mut span);
-            Ok(out_data.unwrap())
-        },
-        Err(err) => Err(err.to_string()),
+impl<'a> StatefulParser<'a> {
+    fn new(data: &'a str) -> Self {
+        Self { span: Span::from(data) }
+    }
+
+    fn parse<T>(&mut self, parser: impl FnMut(Span<'a>) -> IResult<Span<'a>, T>) -> Result<T, String>
+    {
+        let data = std::mem::replace(&mut self.span, Span::from(""));
+        let mut out_data = None;
+        let result = apply((
+            keep(&mut out_data, parser),
+            skip(space0),
+        ))(data);
+    
+        match result {
+            Ok((mut span, _)) => {
+                std::mem::swap(&mut self.span, &mut span);
+                Ok(out_data.unwrap())
+            },
+            Err(err) => Err(err.to_string()),
+        }
     }
 }
+
